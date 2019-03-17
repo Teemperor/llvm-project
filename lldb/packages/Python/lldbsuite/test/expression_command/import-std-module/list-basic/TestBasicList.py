0ca8c6b120a655262cc5aca8eacc68363564e8b2
Raphael Isemann <teemperor@gmail.com>
2019-03-17 12:34:03 +0100

    Fixed some naming

--- lldb/packages/Python/lldbsuite/test/expression_command/import-std-module/list-basic/TestBasicList.py
+++ lldb/packages/Python/lldbsuite/test/expression_command/import-std-module/list-basic/TestBasicList.py
@@ -6,7 +6,7 @@ from lldbsuite.test.decorators import *
 from lldbsuite.test.lldbtest import *
 from lldbsuite.test import lldbutil
 
-class TestListBasic(TestBase):
+class TestBasicList(TestBase):
 
     mydir = TestBase.compute_mydir(__file__)
 
