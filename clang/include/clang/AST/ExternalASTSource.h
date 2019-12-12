b9bf5b0386af708ef8f071c9fe2d68d80ba9644a
Raphael Isemann <teemperor@gmail.com>
2019-12-12 08:56:07 +0100

    [clang][NFC] Move ExternalASTSource RTTI to ID variable
    Remove friend declaration

--- clang/include/clang/AST/ExternalASTSource.h
+++ clang/include/clang/AST/ExternalASTSource.h
@@ -60,15 +60,12 @@ class TagDecl;
 /// actual type and declaration nodes, and read parts of declaration
 /// contexts.
 class ExternalASTSource : public RefCountedBase<ExternalASTSource> {
-  friend class ExternalSemaSource;
-
   /// Generation number for this external AST source. Must be increased
   /// whenever we might have added new redeclarations for existing decls.
   uint32_t CurrentGeneration = 0;
 
-  /// Whether this AST source also provides information for
-  /// semantic analysis.
-  bool SemaSource = false;
+  // LLVM-style RTTI.
+  static char ID;
 
 public:
   ExternalASTSource() = default;
@@ -324,6 +321,10 @@ public:
   }
 
   virtual void getMemoryBufferSizes(MemoryBufferSizes &sizes) const;
+
+  // LLVM-style RTTI.
+  virtual bool isA(const void *ClassID) const { return ClassID == &ID; }
+  static bool classof(const ExternalASTSource *S) { return S->isA(&ID); }
 
 protected:
   static DeclContextLookupResult
