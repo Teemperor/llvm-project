41eef258e506d2ef441a32462c20dc614f3013db
Raphael Isemann <teemperor@gmail.com>
2019-12-13 17:53:09 +0100

    [iwyu-tag] Removed include

--- lldb/source/Initialization/SystemInitializerCommon.cpp
+++ lldb/source/Initialization/SystemInitializerCommon.cpp
@@ -10,7 +10,6 @@
 
 #include "Plugins/Process/gdb-remote/ProcessGDBRemoteLog.h"
 #include "lldb/Host/FileSystem.h"
-#include "lldb/Host/Host.h"
 #include "lldb/Host/HostInfo.h"
 #include "lldb/Host/Socket.h"
 #include "lldb/Utility/Log.h"
