d04ab759bdef89fdd61b74454f0506915b0f49cd
Raphael Isemann <teemperor@gmail.com>
2019-12-13 15:11:21 +0100

    [iwyu-tag] Removed include

--- lldb/source/API/SBWatchpoint.cpp
+++ lldb/source/API/SBWatchpoint.cpp
@@ -10,7 +10,6 @@
 #include "SBReproducerPrivate.h"
 #include "lldb/API/SBAddress.h"
 #include "lldb/API/SBDebugger.h"
-#include "lldb/API/SBDefines.h"
 #include "lldb/API/SBEvent.h"
 #include "lldb/API/SBStream.h"
 
