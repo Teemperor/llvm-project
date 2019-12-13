7d791801e10e56c24b74e14ba59ab835fe5d7376
Raphael Isemann <teemperor@gmail.com>
2019-12-13 15:55:10 +0100

    [iwyu-tag] Removed include

--- lldb/source/Commands/CommandObjectLog.cpp
+++ lldb/source/Commands/CommandObjectLog.cpp
@@ -17,7 +17,6 @@
 #include "lldb/Interpreter/Options.h"
 #include "lldb/Symbol/LineTable.h"
 #include "lldb/Symbol/ObjectFile.h"
-#include "lldb/Symbol/SymbolFile.h"
 #include "lldb/Utility/Args.h"
 #include "lldb/Utility/FileSpec.h"
 #include "lldb/Utility/Log.h"
