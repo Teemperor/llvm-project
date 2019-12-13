2ff344c3942dce0b4cbac829da1986b1d988bcd0
Raphael Isemann <teemperor@gmail.com>
2019-12-13 16:21:38 +0100

    [iwyu-tag] Removed include

--- lldb/source/Commands/CommandObjectSource.cpp
+++ lldb/source/Commands/CommandObjectSource.cpp
@@ -15,7 +15,6 @@
 #include "lldb/Core/SourceManager.h"
 #include "lldb/Host/OptionParser.h"
 #include "lldb/Interpreter/CommandCompletions.h"
-#include "lldb/Interpreter/CommandInterpreter.h"
 #include "lldb/Interpreter/CommandReturnObject.h"
 #include "lldb/Interpreter/OptionArgParser.h"
 #include "lldb/Interpreter/Options.h"
