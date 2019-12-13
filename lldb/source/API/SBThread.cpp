c93bb3f2e28058d6c3bd94fa43f8427a030c60b3
Raphael Isemann <teemperor@gmail.com>
2019-12-13 14:48:24 +0100

    [iwyu-tag] Removed include

--- lldb/source/API/SBThread.cpp
+++ lldb/source/API/SBThread.cpp
@@ -17,7 +17,6 @@
 #include "lldb/API/SBProcess.h"
 #include "lldb/API/SBStream.h"
 #include "lldb/API/SBStructuredData.h"
-#include "lldb/API/SBSymbolContext.h"
 #include "lldb/API/SBThreadCollection.h"
 #include "lldb/API/SBValue.h"
 #include "lldb/Breakpoint/BreakpointLocation.h"
