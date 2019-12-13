e5ff252ff57e10aa3533d7b7df131626be35f364
Raphael Isemann <teemperor@gmail.com>
2019-12-13 15:38:28 +0100

    [iwyu-tag] Removed include

--- lldb/source/Commands/CommandObjectExpression.cpp
+++ lldb/source/Commands/CommandObjectExpression.cpp
@@ -28,7 +28,6 @@
 #include "lldb/Target/Process.h"
 #include "lldb/Target/StackFrame.h"
 #include "lldb/Target/Target.h"
-#include "lldb/Target/Thread.h"
 
 using namespace lldb;
 using namespace lldb_private;
