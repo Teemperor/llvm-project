c166c4264552b0cad04851c85cb9a73632eb8a98
Raphael Isemann <teemperor@gmail.com>
2019-12-13 14:16:24 +0100

    [iwyu-tag] Removed include

--- lldb/source/API/SBDeclaration.cpp
+++ lldb/source/API/SBDeclaration.cpp
@@ -10,7 +10,6 @@
 #include "SBReproducerPrivate.h"
 #include "Utils.h"
 #include "lldb/API/SBStream.h"
-#include "lldb/Host/PosixApi.h"
 #include "lldb/Symbol/Declaration.h"
 #include "lldb/Utility/Stream.h"
 
