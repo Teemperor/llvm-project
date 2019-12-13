0d24e3a166d9e4f077aacae0ce643d5ff649dc2a
Raphael Isemann <teemperor@gmail.com>
2019-12-13 15:10:10 +0100

    [iwyu-tag] Removed include

--- lldb/source/API/SBValue.cpp
+++ lldb/source/API/SBValue.cpp
@@ -18,7 +18,6 @@
 
 #include "lldb/Breakpoint/Watchpoint.h"
 #include "lldb/Core/Module.h"
-#include "lldb/Core/Section.h"
 #include "lldb/Core/ValueObject.h"
 #include "lldb/DataFormatters/DataVisualization.h"
 #include "lldb/Symbol/Declaration.h"
