6bb594c23b2c7230f98e48b5341cde7ecc4fcd2c
Raphael Isemann <teemperor@gmail.com>
2019-12-13 17:07:23 +0100

    [iwyu-tag] Removed include

--- lldb/source/Expression/FunctionCaller.cpp
+++ lldb/source/Expression/FunctionCaller.cpp
@@ -20,7 +20,6 @@
 #include "lldb/Target/Process.h"
 #include "lldb/Target/RegisterContext.h"
 #include "lldb/Target/Target.h"
-#include "lldb/Target/Thread.h"
 #include "lldb/Target/ThreadPlan.h"
 #include "lldb/Target/ThreadPlanCallFunction.h"
 #include "lldb/Utility/Log.h"
