373cfc10ed87c759aa898e09c53b3a1824680fd0
Raphael Isemann <teemperor@gmail.com>
2019-12-13 15:47:08 +0100

    [iwyu-tag] Removed include

--- lldb/source/Commands/CommandObjectFrame.cpp
+++ lldb/source/Commands/CommandObjectFrame.cpp
@@ -8,7 +8,6 @@
 #include "CommandObjectFrame.h"
 #include "lldb/Core/Debugger.h"
 #include "lldb/Core/Module.h"
-#include "lldb/Core/StreamFile.h"
 #include "lldb/Core/ValueObject.h"
 #include "lldb/DataFormatters/DataVisualization.h"
 #include "lldb/DataFormatters/ValueObjectPrinter.h"
