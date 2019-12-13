26a39b1c9b8635515b463de647fe169221b50998
Raphael Isemann <teemperor@gmail.com>
2019-12-13 14:51:56 +0100

    [iwyu-tag] Removed include

--- lldb/source/API/SBThreadPlan.cpp
+++ lldb/source/API/SBThreadPlan.cpp
@@ -39,7 +39,6 @@
 #include "lldb/API/SBAddress.h"
 #include "lldb/API/SBDebugger.h"
 #include "lldb/API/SBEvent.h"
-#include "lldb/API/SBFrame.h"
 #include "lldb/API/SBThreadPlan.h"
 
 #include <memory>
