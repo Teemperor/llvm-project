c7c11fc9f9dbd5ba21eefc9a1f3511aa6bd2aa3d
Raphael Isemann <teemperor@gmail.com>
2019-12-13 14:28:44 +0100

    [iwyu-tag] Removed include

--- lldb/source/API/SBModule.cpp
+++ lldb/source/API/SBModule.cpp
@@ -16,7 +16,6 @@
 #include "lldb/API/SBSymbolContextList.h"
 #include "lldb/Core/Module.h"
 #include "lldb/Core/Section.h"
-#include "lldb/Core/ValueObjectList.h"
 #include "lldb/Core/ValueObjectVariable.h"
 #include "lldb/Symbol/ObjectFile.h"
 #include "lldb/Symbol/SymbolFile.h"
