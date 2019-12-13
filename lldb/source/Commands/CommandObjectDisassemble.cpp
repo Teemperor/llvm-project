032019b80d99fff00ccc8a72625d9a174bd60eea
Raphael Isemann <teemperor@gmail.com>
2019-12-13 15:35:59 +0100

    [iwyu-tag] Removed include

--- lldb/source/Commands/CommandObjectDisassemble.cpp
+++ lldb/source/Commands/CommandObjectDisassemble.cpp
@@ -12,7 +12,6 @@
 #include "lldb/Core/Module.h"
 #include "lldb/Core/SourceManager.h"
 #include "lldb/Host/OptionParser.h"
-#include "lldb/Interpreter/CommandCompletions.h"
 #include "lldb/Interpreter/CommandInterpreter.h"
 #include "lldb/Interpreter/CommandReturnObject.h"
 #include "lldb/Interpreter/OptionArgParser.h"
