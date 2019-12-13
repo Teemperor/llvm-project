de23aeb0b3aad1a26607e3518d62ea65e5ff9c3c
Raphael Isemann <teemperor@gmail.com>
2019-12-13 14:57:09 +0100

    [iwyu-tag] Removed include

--- lldb/source/API/SBTypeEnumMember.cpp
+++ lldb/source/API/SBTypeEnumMember.cpp
@@ -9,7 +9,6 @@
 #include "lldb/API/SBTypeEnumMember.h"
 #include "SBReproducerPrivate.h"
 #include "Utils.h"
-#include "lldb/API/SBDefines.h"
 #include "lldb/API/SBStream.h"
 #include "lldb/API/SBType.h"
 #include "lldb/Symbol/Type.h"
