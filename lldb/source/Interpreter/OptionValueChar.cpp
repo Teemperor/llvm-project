c8cdcbc94be28aaa843868b15ab59dbf50b89877
Raphael Isemann <teemperor@gmail.com>
2019-12-13 18:03:15 +0100

    [iwyu-tag] Removed include

--- lldb/source/Interpreter/OptionValueChar.cpp
+++ lldb/source/Interpreter/OptionValueChar.cpp
@@ -11,7 +11,6 @@
 #include "lldb/Interpreter/OptionArgParser.h"
 #include "lldb/Utility/Stream.h"
 #include "lldb/Utility/StringList.h"
-#include "llvm/ADT/STLExtras.h"
 
 using namespace lldb;
 using namespace lldb_private;
