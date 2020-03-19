1d5efc716999ca09bafc377995af80b47982f550
Raphael Isemann <teemperor@gmail.com>
2020-03-19 17:40:53 +0100

    [lldb][NFC] Don't keep a failed UserExpression around when attempting fixits

--- lldb/source/Expression/UserExpression.cpp
+++ lldb/source/Expression/UserExpression.cpp
@@ -259,6 +259,9 @@ UserExpression::Evaluate(ExecutionContext &exe_ctx,
 
   // If there is a fixed expression, try to parse it:
   if (!parse_success) {
+    // Delete the failed expression that failed to parse.
+    user_expression_sp.reset();
+
     execution_results = lldb::eExpressionParseError;
     if (fixed_expression && !fixed_expression->empty() &&
         options.GetAutoApplyFixIts()) {
