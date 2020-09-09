014b306caf80a0999c7cb319329e0b6e04c19923
Raphael Isemann <teemperor@gmail.com>
2020-09-09 18:23:20 +0200

    [lldb] Assert when TypeSystemClang::CreateFunctionDeclaration is called on a record
    As discussed in D85993, using CreateFunctionDeclaration to add a member function to a record will probably
    cause Clang to run into several assertions and is most likely not what one wants to do. All functions in a
    record are CXXMethodDecls and never plain FunctionDecls. Not sure if there is an obscure Clang extension
    that would support free functions inside a record, but I'm sure we don't have this use case when building an AST in LLDB.
    
    Reviewed By: labath
    
    Differential Revision: https://reviews.llvm.org/D86054

--- lldb/source/Plugins/TypeSystem/Clang/TypeSystemClang.cpp
+++ lldb/source/Plugins/TypeSystem/Clang/TypeSystemClang.cpp
@@ -1999,6 +1999,8 @@ FunctionDecl *TypeSystemClang::CreateFunctionDeclaration(
   ASTContext &ast = getASTContext();
   if (!decl_ctx)
     decl_ctx = ast.getTranslationUnitDecl();
+  assert(!decl_ctx->isRecord() &&
+         "Use AddMethodToCXXRecordType to add member functions to a record");
 
   const bool hasWrittenPrototype = true;
   const bool isConstexprSpecified = false;
