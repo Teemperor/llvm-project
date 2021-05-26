c1f4b73bdea0a41e25b65e583dbd32e9bc33f569
Raphael Isemann <teemperor@gmail.com>
2021-05-26 17:22:23 +0200

    [lldb] iterators

--- lldb/source/Plugins/SymbolFile/DWARF/DWARFDIE.h
+++ lldb/source/Plugins/SymbolFile/DWARF/DWARFDIE.h
@@ -11,9 +11,11 @@
 
 #include "DWARFBaseDIE.h"
 #include "llvm/ADT/SmallSet.h"
+#include "llvm/ADT/iterator_range.h"
 
 class DWARFDIE : public DWARFBaseDIE {
 public:
+  class child_iterator;
   using DWARFBaseDIE::DWARFBaseDIE;
 
   // Tests
@@ -88,6 +90,35 @@ public:
                             int &decl_line, int &decl_column, int &call_file,
                             int &call_line, int &call_column,
                             lldb_private::DWARFExpression *frame_base) const;
+  template<typename T = child_iterator>
+  llvm::iterator_range<T> children() const {
+    return llvm::make_range(T(*this), T());
+  }
+};
+
+class DWARFDIE::child_iterator : public llvm::iterator_facade_base<DWARFDIE::child_iterator, std::forward_iterator_tag, DWARFDIE> {
+  DWARFDIE m_die;
+public:
+  child_iterator() = default;
+  child_iterator(const DWARFDIE &parent) : m_die(parent.GetFirstChild()) {}
+  bool operator==(const child_iterator &it) const {
+    if (!m_die.IsValid() && !it.m_die.IsValid())
+      return true;
+    return m_die == it.m_die;
+  }
+  const DWARFDIE &operator*() const {
+    assert(m_die.IsValid() && "Derefencing invalid iterator?");
+    return m_die;
+  }
+  DWARFDIE &operator*() {
+    assert(m_die.IsValid() && "Derefencing invalid iterator?");
+    return m_die;
+  }
+  child_iterator &operator++() {
+    assert(m_die.IsValid() && "Incrementing invalid iterator?");
+    m_die = m_die.GetSibling();
+    return *this;
+  }
 };
 
 #endif // LLDB_SOURCE_PLUGINS_SYMBOLFILE_DWARF_DWARFDIE_H
