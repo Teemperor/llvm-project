3898884e866d0c9f98168ecb28b3f921e588b6e0
Raphael Isemann <rise@apple.com>
2026-06-08 14:05:48 +0100

    [lldb] Dummy classes

---
+++ lldb/source/Plugins/TypeSystem/Cpp/Decls.cpp
@@ -0,0 +1,1 @@
+#include "CppNodes.h"
---
+++ lldb/source/Plugins/TypeSystem/Cpp/Identifier.h
@@ -0,0 +1,35 @@
+//===-- CppNodes.h ----------------------------------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLDB_SOURCE_PLUGINS_TYPESYSTEM_CPP_IDENTIFIER_H
+#define LLDB_SOURCE_PLUGINS_TYPESYSTEM_CPP_IDENTIFIER_H
+
+#include "lldb/Symbol/CompilerType.h"
+#include "lldb/Symbol/TypeSystem.h"
+
+#include "llvm/ADT/SmallVector.h"
+
+#include <vector>
+
+namespace lldb_private {
+namespace cpp_typesystem {
+
+/// Turns strings into unique IDs.
+class IdentifierMap {
+
+};
+
+///
+class Identifier {
+
+};
+
+}
+}
+
+#endif // LLDB_SOURCE_PLUGINS_TYPESYSTEM_CPP_IDENTIFIER_H
