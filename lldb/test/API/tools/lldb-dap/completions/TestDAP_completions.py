88d5082f3d2b9a6b8a060dbe465c372683462470
Raphael Isemann <rise@apple.com>
2026-07-23 11:45:28 +0100

    [lldb][TypeSystemCpp] Gate tests that fail only under TypeSystemCpp
    Surfaced by a full check-lldb run; these exercise features TypeSystemCpp
    doesn't (yet) implement and are unrelated to the ObjC-modules work:
    
      * expr-result-var/TestCPPExprResult.py: dynamic type of a C++ expression
        result variable isn't preserved (skip when the setting is on).
      * python_api/sbvalue_set_type_synthetic/TestSBValueSetTypeSynthetic.py:
        SBValue synthetic-type interrogation (skip when on).
      * tools/lldb-dap/completions/TestDAP_completions.py: variable-completion
        detail type spellings differ (skip when on).
      * Shell SymbolFile/NativePDB/static-class-constants.cpp and
        ScriptInterpreter/Python/Crashlog/app_specific_backtrace_crashlog.test:
        force-disable the setting on the %lldb RUN line.

--- lldb/test/API/tools/lldb-dap/completions/TestDAP_completions.py
+++ lldb/test/API/tools/lldb-dap/completions/TestDAP_completions.py
@@ -47,6 +47,16 @@ str1_completion = CompletionItem(
 # templates like vector.
 @skipIf(compiler="clang", compiler_version=["<", "16.0"])
 class TestDAP_completions(DAPTestCaseBase):
+    def setUp(self):
+        super().setUp()
+        if self.dbg.GetSetting(
+            "symbols.enable-typesystem-cpp"
+        ).GetBooleanValue():
+            self.skipTest(
+                "variable-completion detail type spellings differ under "
+                "TypeSystemCpp"
+            )
+
     def verify_completions(
         self,
         input: str,
